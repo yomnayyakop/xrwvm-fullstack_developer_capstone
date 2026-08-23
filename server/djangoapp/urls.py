b2dade6641976c14be15cda2d3958eb8ca4988ee
Yomna <yomnayyakop@gmail.com>
2026-08-23 22:25:26 +0300

    Add dealer-related URL paths to urlpatterns

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -12,4 +12,9 @@ urlpatterns = [
     # path for registration
     path(route='register', view=views.registration, name='register'),
     path(route='get_cars', view=views.get_cars, name='getcars'),
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+        path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    path(route='add_review', view=views.add_review, name='add_review' ),
 ]

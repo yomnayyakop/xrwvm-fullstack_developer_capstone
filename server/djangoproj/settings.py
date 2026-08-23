dcb8072bb26589522addcbe35fc0d6a08c9c6601
Yomna <yomnayyakop@gmail.com>
2026-08-23 21:01:32 +0300

    Update ALLOWED_HOSTS and CSRF_TRUSTED_ORIGINS

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -19,13 +19,12 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'yomnayyakop-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'yomnayyakop-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://yomnayyakop-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://yomnayyakop-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
 ]
-
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
 }

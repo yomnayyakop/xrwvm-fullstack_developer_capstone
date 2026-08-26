47dd6e3395e0087334c676fb44b094e10bc47c67
Yomna <yomnayyakop@gmail.com>
2026-08-27 02:21:58 +0300

    Refactor URL patterns for dealer-related views
    Updated URL patterns for dealer reviews and added review paths.

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -2,19 +2,37 @@ from django.urls import path
 from . import views
 
 app_name = 'djangoapp'
+
 urlpatterns = [
-    # path for login
     path(route='login', view=views.login_user, name='login'),
-    
-    # path for logout
     path(route='logout', view=views.logout_request, name='logout'),
-    
-    # path for registration
     path(route='register', view=views.registration, name='register'),
+
     path(route='get_cars', view=views.get_cars, name='getcars'),
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
-        path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
-    path(route='add_review', view=views.add_review, name='add_review' ),
+
+    path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
+    path(
+        route='get_dealers/<str:state>',
+        view=views.get_dealerships,
+        name='get_dealers_by_state'
+    ),
+
+    path(
+        route='dealer/<int:dealer_id>',
+        view=views.get_dealer_details,
+        name='dealer_details'
+    ),
+    
+
+    path(
+        route='reviews/dealer/<int:dealer_id>',
+        view=views.get_dealer_reviews,
+        name='dealer_reviews'
+    ),
+
+    path(
+        route='add_review/',
+        view=views.add_review,
+        name='add_review'
+    ),
 ]

57ef0d8178a9be512edcd8627ea6f5524a829818
Yomna <yomnayyakop@gmail.com>
2026-08-27 02:23:00 +0300

    Add csrf_exempt decorator to add_review view

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -142,6 +142,7 @@ def get_dealer_details(request, dealer_id):
 # Create a `add_review` view to submit a review
 # def add_review(request):
 # ...
+@csrf_exempt
 def add_review(request):
     if(request.user.is_anonymous == False):
         data = json.loads(request.body)
